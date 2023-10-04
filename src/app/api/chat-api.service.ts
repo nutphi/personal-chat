16648442f68b527f129fba90da98293f0103fa87
Nuttakit Phichitsakuldes <nutphi@outlook.com>
2023-10-03 21:00:22 -0400

    feat: split chat api and chat service

---
+++ src/app/api/chat-api.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, of } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { IChatApiService } from './chat-api.interface.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatApiService implements IChatApiService {
+
+  constructor(private http: HttpClient) { }
+
+  getDefaultMessage(): Observable<string> {
+    return this.http.get(`${environment.apiUrl}/practice/`,
+        {responseType: 'text'}
+      )
+      .pipe(catchError(() => of("Unfortunately, I can't answer this question.")));
+  }
+
+  getMessage (message: string): Observable<string> {
+    return this.http.get(`${environment.apiUrl}/practice/new`,
+        {params: {message: encodeURIComponent(message)}, responseType: 'text'}
+      )
+      .pipe(catchError(() => of("Unfortunately, I can't answer this question.")));
+  }
+}
